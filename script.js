945fd5088b7532cf2e4dbd88515ea8a3197b057b
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2024-10-01 19:54:21 +0200

    removed unused files

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('20240924s.md')
-        .then(response => response.text())
-        .then(text => {
-            document.getElementById('content').innerHTML = marked(text);
-        });
-});
